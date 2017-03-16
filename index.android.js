d303f8c2ee4f5e9095754b3270de343db35e0c3b
Rafael Augusto <rafa.2098@gmail.com>
2017-03-16 13:19:30 -0300

    HTTP Axios

--- index.android.js
+++ index.android.js
@@ -8,19 +8,7 @@ import {
 import ListaItens from './src/components/ListaItens'
 
 class app4 extends Component {
-  constructor(props){
-    super(props);
-    console.log('Construindo aplicação');
-  }
-
-  componentWillMount(){
-    console.log('Fazer alguma coisa antes de renderizar');
-  }
-
-  componentDidMount(){
-    console.log('Fazer alguma coisa depois de renderizar');
-  }
-
+  
   render(){
     console.log('Objeto é renderizado');
     return (
--- src/components/ListaItens.js
+++ src/components/ListaItens.js
@@ -1,14 +1,29 @@
 import React, { Component } from 'react';
 import {
-  Text
+  View
 } from 'react-native';
+import axios from 'axios';
+import Itens from './Itens'
 
 class ListaItens extends Component {
-  render() {
-    return (
-      <Text>Teste</Text>
-    );
-  }
+	constructor(props){
+		super(props);
+		this.state = {listaItens: []};
+	}
+
+	componentWillMount(){
+		// requisição HTTP 
+		axios.get('http://faus.com.br/recursos/c/dmairr/api/itens.html')
+			.then(response => { this.setState({listaItens: response.data}) })
+			.catch(() => {console.log('Erro ao recuperar os dados')});
+	}
+	render() {
+		return (
+			<View>
+		      {this.state.listaItens.map(item => { console.log(item) })}
+		    </View>  
+		);
+	}
 }
 
 export default ListaItens;
